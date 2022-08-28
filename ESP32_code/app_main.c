2bd1dd8754829080197c82313945c0197998e900
duydev294 <62111823+duydev294@users.noreply.github.com>
2022-08-29 02:06:30 +0700

    Add files via upload

---
+++ ESP32_code/app_main.c
@@ -0,0 +1,132 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+#include "driver/gpio.h"
+
+static const char *TAG = "MQTT_STATUS";
+static esp_mqtt_client_handle_t client;
+
+
+#define device_ID "0931b3cc-bdd4-4bc9-a2b1-a18eb1e15ff7"
+#define MQTT_Broker "mqtt://mqtt.innoway.vn"
+#define Led 2
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+static void Led_init(void){
+	gpio_config_t io_conf;
+	io_conf.pin_bit_mask = 1<< GPIO_NUM_2;
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	io_conf.pull_up_en = 0;
+	io_conf.pull_down_en = 1;
+	io_conf.intr_type = GPIO_INTR_DISABLE;
+	gpio_config(&io_conf);
+}
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+	char *data;
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, "messages/0931b3cc-bdd4-4bc9-a2b1-a18eb1e15ff7/update", "{\"st\":1}", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+//        msg_id = esp_mqtt_client_subscribe(client, "message/0931b3cc-bdd4-4bc9-a2b1-a18eb1e15ff7/status", 0);
+//        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        esp_mqtt_client_subscribe(client,"messages/0931b3cc-bdd4-4bc9-a2b1-a18eb1e15ff7/status", 0);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        data = event->data;
+        printf("\n%d",*(data+7)-48);
+        if (*(data+6)-48)gpio_set_level(Led,0);
+
+        else gpio_set_level(Led,1);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = MQTT_Broker,
+		.password = "6QgahY8xHd3ud8Oyyh4rtMaRTIUxI8Tl",
+		.username = "VHT_DEV"
+    };
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
+
+void app_main(void)
+{
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    Led_init();
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+
+    ESP_ERROR_CHECK(example_connect());
+
+    mqtt_app_start();
+}
